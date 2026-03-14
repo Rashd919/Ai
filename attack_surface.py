3271d3bf198ac4cb8ed29b563f055ae2be549b59
Rashd919 <hhh123rrhhh@gmail.com>
2026-03-15 01:19:02 +0300

    Update attack_surface.py

--- attack_surface.py
+++ attack_surface.py
@@ -1,18 +1,16 @@
-# attack_surface.py
-# رسم خريطة سطح الهجوم للدومينات الفرعية
-
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def draw_graph(domain, subdomains):
-    """إنشاء خريطة بصريّة لسطح الهجوم"""
+def draw_graph(domain, subs):
+    """رسم شبكة النطاقات الفرعية والسطح الهجومي"""
     G = nx.Graph()
     G.add_node(domain)
-    for sub, ip in subdomains.items():
+    for sub, ip in subs.items():
+        G.add_node(sub)
         G.add_edge(domain, sub)
-    plt.figure(figsize=(8,6))
-    nx.draw_circular(G, with_labels=True, node_color="#FF0000", node_size=1000, font_size=10)
-    path = f"/tmp/{domain}_map.png"
-    plt.savefig(path)
+    plt.figure(figsize=(10,6))
+    nx.draw(G, with_labels=True, node_color='skyblue', node_size=2000, font_size=10, font_weight='bold')
+    file = f"{domain}_attack_surface.png"
+    plt.savefig(file)
     plt.close()
-    return path
+    return file
